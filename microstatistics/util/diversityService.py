9a99c353c8632b259325fa48a52bf77aafd952bd
Gelu Oltean <geluoltean8@gmail.com>
2019-06-19 22:37:53 +0300

    Add type hints. Make methods static.

--- microstatistics/util/diversityService.py
+++ microstatistics/util/diversityService.py
@@ -1,7 +1,9 @@
+import numpy
 from pandas import DataFrame, Series
 from scipy.cluster import hierarchy
 from scipy.spatial import distance
 from sklearn.manifold import MDS
+from typing import Dict
 
 from microstatistics.util.diversities import df_shannon, df_fisher, df_simpson, df_equitability, df_hurlbert, \
     df_proportion, df_bfoi
@@ -25,48 +27,56 @@ class DiversityService(object):
     def __init__(self) -> None:
         super().__init__()
 
-    def compute_index(self, data: DataFrame, strategy: str, size: int = 100) -> DataFrame:
-        if strategy == self.FISHER:
-            return data.apply(self.diversities[self.FISHER])
+    @staticmethod
+    def compute_index(data: DataFrame, strategy: str, size: int = 100) -> DataFrame:
+        if strategy == DiversityService.FISHER:
+            return data.apply(self.diversities[DiversityService.FISHER])
 
-        elif strategy == self.SIMPSON:
-            return data.apply(self.diversities[self.SIMPSON])
+        elif strategy == DiversityService.SIMPSON:
+            return data.apply(DiversityService.diversities[DiversityService.SIMPSON])
 
-        elif strategy == self.SHANNON:
-            return data.apply(self.diversities[self.SHANNON])
+        elif strategy == DiversityService.SHANNON:
+            return data.apply(DiversityService.diversities[DiversityService.SHANNON])
 
-        elif strategy == self.EQUITABILITY:
-            return data.apply(self.diversities[self.EQUITABILITY])
+        elif strategy == DiversityService.EQUITABILITY:
+            return data.apply(DiversityService.diversities[DiversityService.EQUITABILITY])
 
-        elif strategy == self.HURLBERT:
-            return data.apply(lambda x: self.diversities[self.HURLBERT](x, size))
+        elif strategy == DiversityService.HURLBERT:
+            return data.apply(lambda x: DiversityService.diversities[DiversityService.HURLBERT](x, size))
 
         else:
-            raise ValueError(f"Strategy not found. Please select one from the {self.__class__.__name__} constants.")
+            raise ValueError(f"Strategy not found. Please select one from the {DiversityService.__class__.__name__} constants.")
 
-    def compute_bfoi(self, data: DataFrame):
+    @staticmethod
+    def compute_bfoi(data: DataFrame) -> DataFrame:
         return data.apply(df_bfoi)
 
-    def compute_percentages(self, data: DataFrame):
+    @staticmethod
+    def compute_percentages(data: DataFrame) -> DataFrame:
         return data.apply(df_proportion).apply(lambda x: x * 100)
 
-    def compute_percentages_series(self, series: Series):
+    @staticmethod
+    def compute_percentages_series(series: Series) -> DataFrame:
         return df_proportion(series).apply(lambda x: x * 100)
 
-    def compute_morphogroups(self, data: DataFrame):
-        return self.compute_percentages(data).transpose()
+    @staticmethod
+    def compute_morphogroups(data: DataFrame) -> DataFrame:
+        return DiversityService.compute_percentages(data).transpose()
 
-    def compute_linkage(self, data: DataFrame):
-        item_distance = distance.pdist(data, metric="braycurtis")
+    @staticmethod
+    def compute_linkage(data: DataFrame) -> numpy.ndarray:
+        item_distance = distance.pdist(data.values, metric="braycurtis")
         return hierarchy.linkage(item_distance, method="average")
 
-    def compute_nmds(self, data: DataFrame, dimensions: int, runs: int):
-        item_distance = distance.pdist(data.T, metric="braycurtis")
+    @staticmethod
+    def compute_nmds(data: DataFrame, dimensions: int, runs: int) -> Dict[str,]:
+        item_distance = distance.pdist(data.T.values, metric="braycurtis")
         square_dist = distance.squareform(item_distance)
         nmds = MDS(n_components=dimensions, metric=False, dissimilarity="precomputed", max_iter=runs, n_init=30)
 
-        pos = nmds.fit(square_dist).embedding_
-        stress = nmds.fit(square_dist).stress_
+        rez = nmds.fit(square_dist)
+        pos = rez.embedding_
+        stress = rez.stress_
         pos0 = pos[:, 0].tolist()
         pos1 = pos[:, 1].tolist()
 
