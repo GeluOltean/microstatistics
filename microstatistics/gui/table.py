40984b1e5a5392c263d51aa98c32421c8de9073f
Gelu Oltean <geluoltean8@gmail.com>
2019-06-20 22:43:07 +0300

    Generated sources formatting, modificatiosn etc

--- microstatistics/gui/table.py
+++ microstatistics/gui/table.py
@@ -9,7 +9,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Table_Window(object):
+class Table_Window(object):
     def setupUi(self, Table_Window):
         Table_Window.setObjectName("Table_Window")
         Table_Window.setEnabled(True)
