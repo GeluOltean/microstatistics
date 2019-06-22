139b8eee69c15d4c438003aae690a718d6108b05
Gelu Oltean <geluoltean8@gmail.com>
2019-06-22 12:59:16 +0300

    Add docs and type hinting to diversity service

--- microstatistics/util/diversityService.py
+++ microstatistics/util/diversityService.py
@@ -24,6 +24,14 @@ class DiversityService(object):
 
     @staticmethod
     def compute_index(data: DataFrame, strategy: str, size: int = 100) -> DataFrame:
+        """
+        Computes an index based on provided strategy.
+
+        :param data: DataFrame containing a sample dataset
+        :param strategy: index selection based on provided strategy
+        :param size: correction size for the indices that require such a thing
+        :return: a reduced DataFrame object with one column
+        """
         if strategy == FISHER:
             return data.apply(DiversityService.diversities[FISHER])
 
@@ -48,27 +56,59 @@ class DiversityService(object):
 
     @staticmethod
     def compute_bfoi(data: DataFrame) -> DataFrame:
+        """
+        Computes the benthic foraminifera oxygenation index according to K. Kaiho.
+
+        :param data: DataFrame containing individuals separated into oxic, suboxic and disoxic categories
+        :return: a reduced DataFrame object with one column where each entry represents the oxygenation index
+        """
         return data.apply(df_bfoi)
 
     @staticmethod
     def compute_percentages(data: DataFrame) -> DataFrame:
+        """
+        Compute the percentage each entry represents in a sample.
+
+        :param data: DataFrame containing a sample dataset
+        :return: a reduced DataFrame object with one column where each entry represents the species percentage for a
+        sample
+        """
         return data.apply(df_proportion).apply(lambda x: x * 100)
 
     @staticmethod
-    def compute_percentages_series(series: Series) -> DataFrame:
-        return df_proportion(series).apply(lambda x: x * 100)
-
-    @staticmethod
     def compute_morphogroups(data: DataFrame) -> DataFrame:
+        """
+        Applies the @compute_percentages function to a DataFrame and transposes the results. Descriptive function to
+        avoid magic .transpose() parameters in the program
+
+        :param data: DataFrame containing a sample dataset
+        :return: a reduced DataFrame object with one column where each entry represents the species percentage for a
+        sample
+        """
         return DiversityService.compute_percentages(data).transpose()
 
     @staticmethod
     def compute_linkage(data: DataFrame) -> numpy.ndarray:
+        """
+        Computes the Bray-Curtis distance matrix, and subsequently the average linkage between samples.
+
+        :param data: DataFrame containing a sample dataset
+        :return: Computed linkage array.
+        """
         item_distance = distance.pdist(data.values, metric="braycurtis")
         return hierarchy.linkage(item_distance, method="average")
 
+    # TODO: Add dto instead of dict
     @staticmethod
     def compute_nmds(data: DataFrame, dimensions: int, runs: int) -> Dict:
+        """
+        Computes non-Metric Multidimensional Scaling on a given dataset.
+
+        :param data: DataFrame containing a sample dataset
+        :param dimensions: number of dimensions to reduce to when executing nmds
+        :param runs: number of runs to use when computing nmds
+        :return: a dict containing the coordinates and stress values to be used for graphing
+        """
         item_distance = distance.pdist(data.T.values, metric="braycurtis")
         square_dist = distance.squareform(item_distance)
         nmds = MDS(n_components=dimensions, metric=False, dissimilarity="precomputed", max_iter=runs, n_init=30)
